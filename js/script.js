034482242f7937a7a3a421d3b245c0c635f7bdf6
E-C-Shackelford <ecs.shackelford@gmail.com>
2022-07-06 18:28:23 -0700

    Fetch random profile data from the random user generator API; parse the data to a JSON file; create an array with data; and create a function expression to populate user data on the page so that 5 users, with their names, countries and images, display

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+  const usersRequest = await fetch("https://randomuser.me/api?results=5");
+  const data = await usersRequest.json();
+  // console.log(data);
+  /* 
+  look at what your data variable returns in the console,
+  find the property name for the array of user objects,
+  create a new variable and map it to that property
+   */
+  const userResults = data.results;
+  // console.log(userResults);
+
+  displayUsers(userResults);
+};
+getData();
+
+const displayUsers = function (userResults) {
+  // empty randomFolks element’s contents to make sure there aren't any duplicated DOM elements
+  randomFolks.innerHTML = "";
+
+  for (const user of userResults) {
+    // for every user, select their country, first name, and avatar URL with a size of medium
+    const country = user.location.country;
+    const name = user.name.first;
+    const imageUrl = user.picture.medium;
+    const userDiv = document.createElement("div");
+    userDiv.innerHTML = `<h3>${name}</h3> <p>${country}</p> 
+    <img src=${imageUrl} alt="User avatar"/>`;
+    randomFolks.append(userDiv);
+  }
+};
